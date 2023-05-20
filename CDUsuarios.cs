a6c3608b4a9ab118867ff4213f0f41fe9a8eae4d
jochu2207 <82623210+jochu2207@users.noreply.github.com>
2023-05-20 00:14:56 -0500

    Add files via upload
    Capa datos Producto y Usuario

---
+++ CDUsuarios.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using CapaEntidad;
+
+namespace CapaDatos
+{
+    public class CDUsuarios
+    {
+
+        public List<Usuario> Listar()
+        {
+            List<Usuario> lista = new List<Usuario>();
+
+            try
+            {
+                using(SqlConnection oconexion = new SqlConnection(Conexion.cn)) {
+
+                    string query = "select IdUsuario, Nombres, Apellidos, Correo, Clave, Reestablecer, Activo from Usuario";
+
+                    SqlCommand cmd = new SqlCommand(query, oconexion);
+                    cmd.CommandType = CommandType.Text;
+
+                    oconexion.Open();
+
+                    using (SqlDataReader dr = cmd.ExecuteReader()) {
+
+                        while (dr.Read())
+                        {
+
+                            lista.Add(
+                                new Usuario()
+                                {
+                                    IdUsuario = Convert.ToInt32(dr["IdUsuario"]),
+                                    Nombres = dr["Nombres"].ToString(),
+                                    Apellidos = dr["Apellidos"].ToString(),
+                                    Correo = dr["Correo"].ToString(),
+                                    Clave = dr["Clave"].ToString(),
+                                    Reestablecer = Convert.ToBoolean(dr["Reestablecer"]),
+                                    Activo = Convert.ToBoolean(dr["Activo"])
+
+                                });
+                        }
+                    } 
+                }
+            }
+            catch
+            {
+                lista = new List<Usuario>();
+            }
+
+            return lista;
+        }
+
+        
+        public int Registrar(Usuario obj, out string Mensaje)
+        {
+
+            int idAutogenerado = 0;
+            Mensaje = string.Empty;
+            try
+            {
+                using (SqlConnection ocon = new SqlConnection(Conexion.cn))
+                {
+                    SqlCommand cmd = new SqlCommand("sp_RegistraUsuario", ocon);
+                    cmd.Parameters.AddWithValue("Nombres", obj.Nombres);
+                    cmd.Parameters.AddWithValue("Apellidos", obj.Apellidos);
+                    cmd.Parameters.AddWithValue("Correo", obj.Correo);
+                    cmd.Parameters.AddWithValue("Clave", obj.Clave);
+                    cmd.Parameters.AddWithValue("Activo", obj.Activo);
+                    cmd.Parameters.Add("Resultado", SqlDbType.Int).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    ocon.Open();
+
+                    cmd.ExecuteNonQuery();
+
+                    idAutogenerado = Convert.ToInt32(cmd.Parameters["Resultado"].Value);
+                    Mensaje = cmd.Parameters["Mensaje"].Value.ToString();
+                }
+
+            }
+            catch(Exception ex)
+            {
+                idAutogenerado = 0;
+                Mensaje = ex.Message;
+
+            }
+            return idAutogenerado;
+
+        }
+
+
+        public bool Editar(Usuario obj, out string Mensaje)
+        {
+            bool resultado = false;
+            Mensaje = string.Empty;
+
+            try
+            {
+                using (SqlConnection ocon = new SqlConnection(Conexion.cn))
+                {
+                    SqlCommand cmd = new SqlCommand("sp_EditarUsuario", ocon);
+                    cmd.Parameters.AddWithValue("IdUsuario", obj.IdUsuario);
+                    cmd.Parameters.AddWithValue("Nombres", obj.Nombres);
+                    cmd.Parameters.AddWithValue("Apellidos", obj.Apellidos);
+                    cmd.Parameters.AddWithValue("Correo", obj.Correo);
+                    cmd.Parameters.AddWithValue("Activo", obj.Activo);
+                    cmd.Parameters.Add("Resultado", SqlDbType.Bit).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    ocon.Open();
+
+                    cmd.ExecuteNonQuery();
+
+                    resultado = Convert.ToBoolean(cmd.Parameters["Resultado"].Value);
+                    Mensaje = cmd.Parameters["Mensaje"].Value.ToString();
+                }
+            }
+            catch(Exception ex)
+            {
+                resultado = false;
+                Mensaje = ex.Message;
+
+            }
+            return resultado;
+        }
+
+
+        public bool Eliminar(int id, out string Mensaje)
+        {
+
+            bool resultado = false;
+            Mensaje = string.Empty;
+            try
+            {
+                using (SqlConnection ocon = new SqlConnection(Conexion.cn))
+                {
+                    SqlCommand cmd = new SqlCommand("delete top (1) from usuario where IdUsuario = @id", ocon);
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.CommandType = CommandType.Text;
+                    ocon.Open();
+                    resultado = cmd.ExecuteNonQuery() > 0 ? true : false;
+                }
+            }
+            catch (Exception ex)
+            {
+                resultado = false;
+                Mensaje = ex.Message;
+
+            }
+
+            return resultado;
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ CD_Producto.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CapaEntidad;
+using System.Data.SqlClient;
+using System.Data;
+using System.Globalization;
+
+namespace CapaDatos
+{
+    public class CD_Producto
+    {
+        public List<Producto> Listar()
+        {
+            List<Producto> list = new List<Producto>();
+
+            try
+            {
+                using (SqlConnection oconex = new SqlConnection(Conexion.cn))
+                {
+                    string query = "sp_ListarProductos";
+
+                    SqlCommand cmd = new SqlCommand(query, oconex);
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    oconex.Open();
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            list.Add(new Producto()
+                            {
+                                IdProducto = Convert.ToInt32(dr["IdProducto"]),
+                                nombre = dr["Nombre"].ToString(),
+                                Descripcion = dr["Descripcion"].ToString(),
+                                oMarcar = new Marca() { IdMarca = Convert.ToInt32(dr["IdMarca"]), Descripcion = dr["DesMarca"].ToString() },
+                                oCategoria = new Categoria() { IdCategoria = Convert.ToInt32(dr["IdCategoria"]), Descripcion = dr["DesCategoria"].ToString() },
+                                Precio = Convert.ToDecimal(dr["Precio"], new CultureInfo("es-PE")),
+                                Stock = Convert.ToInt32(dr["Stock"]),
+                                RutaImagen = dr["RutaImagen"].ToString(),
+                                NombreImagen = dr["NombreImagen"].ToString(),
+                                Activo = Convert.ToBoolean(dr["Activo"])
+
+                            });
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                list = new List<Producto>();
+            }
+            return list;
+        }
+
+
+        public int Registrar(Producto obj, out string Mensaje)
+        {
+            int idAutogen = 0;
+
+            Mensaje = string.Empty;
+
+            try
+            {
+                using (SqlConnection ocn = new SqlConnection(Conexion.cn))
+                {
+                    SqlCommand cmd = new SqlCommand("sp_RegistrarProducto", ocn);
+                    cmd.Parameters.AddWithValue("Nombre", obj.nombre);
+                    cmd.Parameters.AddWithValue("Descripcion", obj.Descripcion);
+                    cmd.Parameters.AddWithValue("IdMarca", obj.oMarcar.IdMarca);
+                    cmd.Parameters.AddWithValue("IdCategoria", obj.oCategoria.IdCategoria);
+                    cmd.Parameters.AddWithValue("Precio", obj.Precio);
+                    cmd.Parameters.AddWithValue("Stock", obj.Stock);
+                    cmd.Parameters.AddWithValue("Activo", obj.Activo);
+                    cmd.Parameters.Add("Resultado", SqlDbType.Int).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    ocn.Open();
+                    cmd.ExecuteNonQuery();
+
+                    idAutogen = Convert.ToInt32(cmd.Parameters["Resultado"].Value);
+                    Mensaje = cmd.Parameters["Mensaje"].Value.ToString();
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                idAutogen = 0;
+                Mensaje = ex.Message;
+            }
+            return idAutogen;
+        }
+
+        public bool Editar(Producto obj, out string Mensaje)
+        {
+            bool resultado = false;
+            Mensaje = string.Empty;
+
+            try
+            {
+                using (SqlConnection ocn = new SqlConnection(Conexion.cn))
+                {
+                    SqlCommand cmd = new SqlCommand("sp_EditarProducto", ocn);
+                    cmd.Parameters.AddWithValue("IdProducto", obj.IdProducto);
+                    cmd.Parameters.AddWithValue("Nombre", obj.nombre);
+                    cmd.Parameters.AddWithValue("Descripcion", obj.Descripcion);
+                    cmd.Parameters.AddWithValue("IdMarca", obj.oMarcar.IdMarca);
+                    cmd.Parameters.AddWithValue("IdCategoria", obj.oCategoria.IdCategoria);
+                    cmd.Parameters.AddWithValue("Precio", obj.Precio);
+                    cmd.Parameters.AddWithValue("Stock", obj.Stock);
+                    cmd.Parameters.AddWithValue("Activo", obj.Activo);
+                    cmd.Parameters.Add("Resultado", SqlDbType.Bit).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    ocn.Open();
+                    cmd.ExecuteNonQuery();
+
+                    resultado = Convert.ToBoolean(cmd.Parameters["Resultado"].Value);
+                    Mensaje = cmd.Parameters["Mensaje"].Value.ToString();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                resultado = false;
+                Mensaje = ex.Message;
+            }
+            return resultado;
+
+        }
+
+        public bool SaveDataImg(Producto obj, out string Mensaje)
+        {
+            bool resultado = false;
+            Mensaje = string.Empty;
+
+            try
+            {
+                using (SqlConnection conec = new SqlConnection(Conexion.cn))
+                {
+                    string query = "Update producto set RutaImagen = @rutaImagen, NombreImagen = @nombreImagen where IdProducto = @IdProducto";
+
+                    SqlCommand cmd = new SqlCommand(query, conec);
+                    cmd.Parameters.AddWithValue("@rutaImagen", obj.RutaImagen);
+                    cmd.Parameters.AddWithValue("@nombreImagen", obj.NombreImagen);
+                    cmd.Parameters.AddWithValue("@IdProducto", obj.IdProducto);
+                    cmd.CommandType = CommandType.Text;
+
+                    conec.Open();
+
+                    if (cmd.ExecuteNonQuery() > 0)
+                    {
+                        resultado = true;
+
+                    }
+                    else
+                    {
+                        Mensaje = "No se pudo actualizar la imagen";
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                resultado = false;
+                Mensaje = ex.Message;
+            }
+            return resultado;
+
+        }
+
+
+        public bool Eliminar(int id, out string Mensaje)
+        {
+            bool resultado = false;
+            Mensaje = string.Empty;
+
+            try
+            {
+                using (SqlConnection conec = new SqlConnection(Conexion.cn))
+                {
+                    SqlCommand cmd = new SqlCommand("sp_EliminarProducto", conec);
+                    cmd.Parameters.AddWithValue("IdProducto", id);
+                    cmd.Parameters.Add("Resultado", SqlDbType.Bit).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    conec.Open();
+
+                    resultado = Convert.ToBoolean(cmd.Parameters["Resultado"].Value);
+                    Mensaje = cmd.Parameters["Mensaje"].Value.ToString();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                resultado = false;
+                Mensaje = ex.Message;
+            }
+            return resultado;
+        }
+
+
+    }
+}
