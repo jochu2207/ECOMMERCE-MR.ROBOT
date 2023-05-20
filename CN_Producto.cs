d1ea612593d47a6bfe340cde4b4c984842245811
jochu2207 <82623210+jochu2207@users.noreply.github.com>
2023-05-20 00:17:56 -0500

    Add files via upload
    Capa Negocio Usuario y Producto

---
+++ CN_Producto.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CapaDatos;
+using CapaEntidad;
+
+namespace CapaNegocio
+{
+    public class CN_Producto
+    {
+       private CD_Producto oCapData = new CD_Producto();
+
+        public List<Producto> Listar()
+        {
+            return oCapData.Listar();
+        }
+
+        public int Registrar(Producto obj, out string Mensaje)
+        {
+            Mensaje = string.Empty;
+
+            if(string.IsNullOrEmpty(obj.nombre) || string.IsNullOrWhiteSpace(obj.nombre))
+            {
+                Mensaje = "Por favor, ingresa el nombre al producto";
+            }
+            else if (string.IsNullOrEmpty(obj.Descripcion) || string.IsNullOrWhiteSpace(obj.Descripcion))
+            {
+                Mensaje = "Por favor, ingresa la descripcion del producto";
+            }
+            else if (obj.oMarcar.IdMarca == 0)
+            {
+                Mensaje = "Por favor, selecciona una Marca";
+            }
+            else if (obj.oCategoria.IdCategoria == 0)
+            {
+                Mensaje = "Por favor, selecciona una Categoria";
+            }
+            else if (obj.Precio == 0)
+            {
+                Mensaje = "Por favor, ingresa el precio del Producto";
+            }
+            else if(obj.Stock == 0)
+            {
+                Mensaje = "Por favor, ingresa el stock del Producto";
+            }
+
+            if (string.IsNullOrEmpty(Mensaje))
+            {
+                return oCapData.Registrar(obj, out Mensaje);
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+
+
+        public bool Editar(Producto obj, out string Mensaje)
+        {
+            Mensaje = string.Empty;
+
+            if (string.IsNullOrEmpty(obj.nombre) || string.IsNullOrWhiteSpace(obj.nombre))
+            {
+                Mensaje = "Por favor, ingresa el nombre del Producto";
+            }
+            else if (string.IsNullOrEmpty(obj.Descripcion) || string.IsNullOrWhiteSpace(obj.Descripcion))
+            {
+                Mensaje = "Por favor, ingresa la descripcion del producto";
+            }
+            else if (obj.oMarcar.IdMarca == 0)
+            {
+                Mensaje = "Por favor, selecciona una Marca";
+            }
+            else if (obj.oCategoria.IdCategoria == 0)
+            {
+                Mensaje = "Por favor, selecciona una Categoria";
+            }
+            else if (obj.Precio == 0)
+            {
+                Mensaje = "Por favor, ingresa el precio del Producto";
+            }
+            else if (obj.Stock == 0)
+            {
+                Mensaje = "Por favor, ingresa el stock del Producto";
+            }
+
+            if (string.IsNullOrEmpty(Mensaje))
+            {
+                return oCapData.Editar(obj, out Mensaje);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        public bool SaveDataImg(Producto obj, out string Mensaje)
+        {
+            return oCapData.SaveDataImg(obj, out Mensaje);
+        }
+
+        public bool Eliminar(int id, out string Mensaje)
+        {
+            return oCapData.Eliminar(id, out Mensaje);
+        }
+
+    }
+}
---
+++ CN_Usuario.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CapaDatos;
+using CapaEntidad;
+
+namespace CapaNegocio
+{
+    public class CN_Usuario
+    {
+
+        private CDUsuarios oCapaDato = new CDUsuarios();
+
+        public List<Usuario> Listar()
+        {
+            return oCapaDato.Listar();
+        }
+
+
+        public int Registrar(Usuario obj, out string Mensaje)
+        {
+
+            Mensaje = string.Empty;
+
+            if(string.IsNullOrEmpty(obj.Nombres) || string.IsNullOrWhiteSpace(obj.Nombres))
+            {
+                Mensaje = "Por favor, ingresa tus nombres";
+            }
+
+            else if (string.IsNullOrEmpty(obj.Apellidos) || string.IsNullOrWhiteSpace(obj.Apellidos))
+            {
+                Mensaje = "Por favor, ingresa tus apellidos";
+            }
+            else if (string.IsNullOrEmpty(obj.Correo) || string.IsNullOrWhiteSpace(obj.Correo))
+            {
+                Mensaje = "Por favor, ingresa un correo";
+            }
+
+            if (string.IsNullOrEmpty(Mensaje))
+            {
+
+                string clave = CN_Recursos.GenerarClave();
+                string asunto = "Registro de cuenta";
+                string msg_mail = "<h3>La cuenta ha sido creada satisfactoriamente</h3></br><p>La contraseña de acceso es: !clave!</p>";
+                msg_mail = msg_mail.Replace("!clave!", clave);
+
+                bool rpta = CN_Recursos.SendMail(obj.Correo, asunto, msg_mail);
+
+                if (rpta)
+                {
+                    obj.Clave = CN_Recursos.ConvertToSha256(clave);
+                    return oCapaDato.Registrar(obj, out Mensaje);
+                }
+                else
+                {
+                    Mensaje = "No se pudo enviar el Correo";
+                    return 0;
+                }
+
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+
+        public bool Editar(Usuario obj, out string Mensaje)
+        {
+            Mensaje = string.Empty;
+
+            if(string.IsNullOrEmpty(obj.Nombres) || string.IsNullOrWhiteSpace(obj.Nombres))
+            {
+                Mensaje = "Este campo no puede ser vacio";
+
+            }
+            else if(string.IsNullOrEmpty(obj.Apellidos) || string.IsNullOrWhiteSpace(obj.Apellidos))
+            {
+                Mensaje = "Este campo no puede ser vacio";
+
+            }
+            else if(string.IsNullOrEmpty(obj.Correo) || string.IsNullOrWhiteSpace(obj.Correo))
+            {
+                Mensaje = "Este campo no puede ser vacio";
+            }
+
+            if (string.IsNullOrEmpty(Mensaje))
+            {
+                return oCapaDato.Editar(obj, out Mensaje);
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+
+        public bool Eliminar(int id, out string Mensaje)
+        {
+            return oCapaDato.Eliminar(id, out Mensaje);
+        }
+
+
+
+
+
+
+
+    }
+}
